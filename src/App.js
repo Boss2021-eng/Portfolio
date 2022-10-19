31c3a57f3c612c6aa8cf108576f14ee6cd2da616
Boss2021-eng <77675650+Boss2021-eng@users.noreply.github.com>
2022-10-19 08:01:32 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import NavBar from './components/NavBar'
-import Home from './components/Home'
-import About from './components/About';
-
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+import Analytics from './components/Analytics';
+import NewsLetter from './components/Newsletter';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
 
 function App() {
  
   return (
     <div> 
        <NavBar />
-       <Home />
-       <About />
+       <Hero />
+       <Analytics />
+       <NewsLetter />
+       <Cards />
+       <Footer />
     </div>
     
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,75 +1,36 @@
-import React, {useState} from 'react'
-import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
-import {HiOutlineMail} from 'react-icons/hi'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
-import Logo from '../assets/logo1.png'
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
 
-const NavBar = () => {
-  const [nav, setNav] = useState('false');
-
-  const handleClick = () => {
+  const handleNav = () => {
     setNav(!nav);
-  }
+  };
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300' >
-      <div> 
-        <img src={Logo} alt="Logo Image" style={{width: '50px'}}/>
-      </div>
-      <div>
-
-        {/* menu */}
-        <ul className='hidden md:flex'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-
-      {/* Hamburger */}
-      <div onClick ={handleClick} className='md:hidden z-10'>
-        {nav? <FaTimes /> :<FaBars />}
-      </div>
-      {/* Mobile Menu */}
-      <ul className={nav? 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center': 'hidden'}>
-          <li className='py-6 text-4xl'>Home</li>
-          <li className='py-6 text-4xl'>About</li>
-          <li className='py-6 text-4xl'>Skills</li>
-          <li className='py-6 text-4xl'>Work</li>
-          <li className='py-6 text-4xl'>Contact</li>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <ul className='hidden md:flex'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>Company</li>
+        <li className='p-4'>Resources</li>
+        <li className='p-4'>About</li>
+        <li className='p-4'>Contact</li>
       </ul>
-
-      {/* Social Icons */}
-      <div className='hidden lg:flex fixed top-[35%] left-0 flex flex-col'>
-        <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center full ml-[-100px] hover:ml-[-10px] duration-300s bg-blue-600'> 
-              <a className='flex justify-between items-center w-full text-gray-300'
-              href='/'>Linkedin <FaLinkedin size={30} /></a>
-          </li>
-
-          <li className='w-[160px] h-[60px] flex justify-between items-center full ml-[-100px] hover:ml-[-10px] duration-300s bg-[#333333]'> 
-              <a className='flex justify-between items-center w-full text-gray-300'
-              href='/'>Github <FaGithub size={30} /></a>
-          </li>
-
-          <li className='w-[160px] h-[60px] flex justify-between items-center full ml-[-100px] hover:ml-[-10px] duration-300s bg-[#6fc2b0]'> 
-              <a className='flex justify-between items-center w-full text-gray-300'
-              href='/'> Email <HiOutlineMail size={30} /></a>
-          </li>
-
-          <li className='w-[160px] h-[60px] flex justify-between items-center full ml-[-100px] hover:ml-[-10px] duration-300s bg-[#565f69]'> 
-              <a className='flex justify-between items-center w-full text-gray-300'
-              href='/'>Resume <BsFillPersonLinesFill size={30} /></a>
-          </li>
-        </ul>
+      <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
-      
-
+      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+          <li className='p-4 border-b border-gray-600'>Home</li>
+          <li className='p-4 border-b border-gray-600'>Company</li>
+          <li className='p-4 border-b border-gray-600'>Resources</li>
+          <li className='p-4 border-b border-gray-600'>About</li>
+          <li className='p-4'>Contact</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default Navbar;
